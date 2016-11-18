a7b4f34ee4f400785ddce9460211dced530cec3e
overclock07 <sunjen_tech@hotmail.com>
2016-11-17 21:12:54 -0500

    Update world.php

--- world.php
+++ world.php
@@ -1,20 +1,33 @@
 <?php
-mysql_connect(
-"0.0.0.0",
-"yahsharahla"
-);
-mysql_select_db("world");
-
-
-$LOOKUP = $_REQUEST['country'];
-
-# execute a SQL query on the database
-$results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
-print $results;
-# loop through each country
-while ($row = mysql_fetch_array($results)) {
+  mysql_connect("0.0.0.0","Overclock07");
+  mysql_select_db("world");
+  $LOOKUP = $_GET['lookup'];
+  $all = $_GET['all'];
+  
+  if(isset($all)){
+    $val = print_r($all);
+    $results = mysql_query("SELECT * FROM countries;");
+  }
+  
+  elseif(isset($LOOKUP)){
+    $val = print_r($LOOKUP);
+    $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
+  }
+  print '<p>'.$results.'</p>';
+  
+  # loop through each country
+  while ($row = mysql_fetch_array($results)){ 
   ?>
-  <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
   <?php
-}
+    if($row["name"] == $val){
+      echo "<p>".$row["name"]." ruled by ".$row["head_of_state"]."</p>";
+    }
+    
+    else{
+      http_response_code(404);
+      break; 
+    }
+  ?>
+  <?php
+  }
 ?>
