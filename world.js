ed9b5563ff6e750518f88a884a9bd177a748df5f
overclock07 <sunjen_tech@hotmail.com>
2016-11-17 21:08:34 -0500

    Update world.js

--- world.js
+++ world.js
@@ -1,18 +1,73 @@
+var term;
+var lookup;
+var box;
+
+//main function
 window.onload = function(){
-    $("lookup").onclick = search_it;
+    lookup = document.getElementById('lookup');
+    box = document.getElementById("box");
     
+    document.getElementById('lookup').onclick = function(){
+        if(term.length < 1){
+            alert("Please enter a country!")
+        }
+        
+        else{
+            new Ajax.Request("world.php",
+            {
+                method: "GET",
+                parameters: {lookup: ''+term},
+                onSuccess: callPHP,
+                onFailure: failureFunc
+            }
+        );
+        }
+    }
+  
+  
+  press();
 };
 
-function search(Jamaica){
-       var data= Jamaica.responseText || "no response text";
-                        alert(data);
-                        $('result').update(data);
+function press(){
+    term = document.getElementById("term").value;
+    
+    if(term.length < 1){
+        alert("Please enter a country!")
+    }
 };
-//something wrong with this code, world1.js is working better
-function search_it(){
-    new Ajax.Request( "world.php",{
-        method: "get",
-        parameters: $('term').serialize(true),
-        onSuccess: search
-    });
+
+function checkbox(){
+    if(box.checked){
+        alert("checked");
+        
+        new Ajax.Request("world.php",
+            {
+                method: "GET",
+                parameters: {all: "true"},
+                onSuccess: callPHP,
+                onFailure: failureFunc
+            }
+        );
+    }
+    
+    if(box.unchecked){
+        document.getElementById("lookup").innerHTML = "Lookup";
+    }
 };
+
+
+function callPHP(responseObject){
+    if(responseObject.status == 200){
+        document.getElementById("result").innerHTML = responseObject.responseText;
+    }
+}
+
+function callXmlPHP(responseObject){
+    if(responseObject.status == 200){
+        document.getElementById("result").innerHTML = responseObject.responseXML;
+    }
+}
+
+function failureFunc(responseObject){
+     alert("Did you press the enter key when finish typing???");
+}
